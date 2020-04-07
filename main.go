ca65176c02b470fc1d5f4d77e8ca573bef869388
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2020-04-08 03:06:21 +0900

    関数分けとモード選択機能の追加

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/url"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/ChimeraCoder/anaconda"
 )
@@ -17,10 +21,10 @@ type TwitterAccount struct {
 }
 
 func main() {
-	raw, error := ioutil.ReadFile("./token.json")
+	raw, err := ioutil.ReadFile("./token.json")
 
-	if error != nil {
-		fmt.Println(error.Error())
+	if err != nil {
+		fmt.Println(err.Error())
 		return
 	}
 
@@ -29,19 +33,61 @@ func main() {
 
 	api := anaconda.NewTwitterApiWithCredentials(account.AccessToken, account.AccessTokenSecret, account.ConsumerKey, account.ConsumerSecret)
 
+	fmt.Println("モードを選んでください:")
+	fmt.Println("1: 全削除, 2: 指定ワードを含むツイートを削除")
+
+	command, err := IntStdin()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	switch command {
+	case 1:
+		fmt.Println("全削除を開始します")
+		DeleteAllTweet(api)
+	case 2:
+		fmt.Println("指定ワードを含むツイートの削除を開始します")
+		DeleteSelectTweet(api)
+	}
+}
+
+func StrStdin() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	s := scanner.Text()
+
+	s = strings.TrimSpace(s)
+	return s
+}
+
+func IntStdin() (int, error) {
+	s := StrStdin()
+	return strconv.Atoi(strings.TrimSpace(s))
+}
+
+func DeleteAllTweet(api *anaconda.TwitterApi) {
 	v := url.Values{}
 	v.Set("count", "200")
 	v.Set("include_rts", "true")
 	var id int64
 
 	for i := 0; i < 16; i++ {
-		tweets, error := api.GetUserTimeline(v)
+		tweets, err := api.GetUserTimeline(v)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 		for _, tweet := range tweets {
 			fmt.Println(tweet.Id)
-			fmt.Println(error)
 			api.DeleteTweet(tweet.Id, true)
 			id = tweet.Id
 		}
 		v.Set("max_id", string(id-1))
 	}
+	fmt.Println("全削除完了！")
+}
+
+func DeleteSelectTweet(api *anaconda.TwitterApi) {
 }
